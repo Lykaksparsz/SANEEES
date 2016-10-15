14151afbb3adffd4e089ef471a988a78b046e1fb
Lykakspars <lykakspars@outlook.com>
2016-10-15 22:53:00 +0300

    Update saneees.js

--- saneees.js
+++ saneees.js
@@ -1,6 +1,6 @@
 var sanimg = {"http://dankmaymays.com/badtime/badtime.jpg", "https://pbs.twimg.com/media/CUlJEU5XIAAxwjT.png", "https://img.ifcdn.com/images/3bd7a03ffabe6eee9aaa0980890c0eb46fe76fbdc1a9cfd631ee865c76636c66_1.jpg", "https://img.ifcdn.com/images/cbdbffa3a2aaa4c531011d06dcfc61bd160fbb89187b0691ac8e425997f68810_1.jpg", "https://img.ifcdn.com/images/980757df4fa1d38388c93f34f886f75adae4746d772d1b3bff8d6a9c191b3568_1.jpg", "https://40.media.tumblr.com/3a7900796e37a8fe0253616d38a0287c/tumblr_nya8avQCMf1ulj9ivo1_500.png", "http://67.media.tumblr.com/c8a998438e76c7a74b78e3736d0b308a/tumblr_nx22553hZX1ukn3fzo1_1280.png", "http://i.imgur.com/fsR6lWB.png", "https://i.ytimg.com/vi/owJi9z6BsWQ/hqdefault.jpg", "http://i3.kym-cdn.com/photos/images/original/001/031/372/a67.jpg"}
 
 function sanes() {
-    var x = Math.floor((Math.random() * 10) + 1);
+    var x = Math.floor((Math.random() * 5) + 1);
     return sanimg[x]
 }
